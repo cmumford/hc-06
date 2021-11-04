0c6be3cbfeb274118d73a7f935d1837ad7e1d598
Chris Mumford <cmumford@cmumford.com>
2021-11-03 20:19:43 -0700

    sim: Improvemenets to HC-06 simulator.

--- simulator/code.py
+++ simulator/code.py
@@ -1,11 +1,29 @@
 import supervisor
-
-print("listening...")
+import sys
 
 while True:
-    if supervisor.runtime.serial_bytes_available:
-        value = input().strip()
-        # Sometimes Windows sends an extra (or missing) newline - ignore them
-        if value == "":
-            continue
-        print("RX: {}".format(value))
+    input_chars = []
+    while supervisor.runtime.serial_bytes_available:
+        input_str = sys.stdin.read(1)
+        input_chars.append(input_str)
+    if len(input_chars):
+        input_cmd = ''.join(input_chars)
+        if input_cmd == 'AT':
+            sys.stdout.write('OK')
+        else:
+            if input_cmd.startswith('AT+'):
+                message = input_cmd[3:]
+                if message == 'VERSION':
+                    sys.stdout.write(' LinvorV1.8')
+                elif message.startswith('BAUD'):
+                    sys.stdout.write('OK1200')
+                elif message.startswith('NAME'):
+                    sys.stdout.write('OKname')
+                elif message.startswith('PIN'):
+                    sys.stdout.write('OKsetpin')
+                elif message in ('PE', 'PO', 'PN'):
+                    sys.stdout.write('OK ODD')
+                else:
+                    sys.stdout.write('Unknown msg: "%s"' % message)
+            else:
+                sys.stdout.write('Unknown: "%s"' % input_cmd)
